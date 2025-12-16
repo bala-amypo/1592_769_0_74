8b03458e98304bcac8d19a0b5ae34ba6d4d05865
bala-amypo <bala@amypo.in>
2025-12-16 03:38:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vehicle.java
+++ src/main/java/com/example/demo/entity/Vehicle.java
@@ -8,5 +8,52 @@ public class Vehicle {
     private Double fuelEfficiency;
 
     public Vehicle(){}
-    
+
+    public Vehicle(User user, String vehicleNumber, Double capacityKg, Double fuelEfficiency) {
+        this.user = user;
+        this.vehicleNumber = vehicleNumber;
+        this.capacityKg = capacityKg;
+        this.fuelEfficiency = fuelEfficiency;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public Double getCapacityKg() {
+        return capacityKg;
+    }
+
+    public void setCapacityKg(Double capacityKg) {
+        this.capacityKg = capacityKg;
+    }
+
+    public Double getFuelEfficiency() {
+        return fuelEfficiency;
+    }
+
+    public void setFuelEfficiency(Double fuelEfficiency) {
+        this.fuelEfficiency = fuelEfficiency;
+    }
+
 }
